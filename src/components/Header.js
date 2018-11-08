3a9733d5bf471e6bb4fd4e733642b024f257560a
Victor Gago <victor.gago@ESCALA2245.local>
2018-11-08 11:48:59 -0200

    Fixed missing image reference / compile error

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
 import './VShop.css';
 
 class Header extends Component {
   render() {
     return (
       <header id="vshop-header">
-        <img src={logo} id="vshop-logo" alt="logo" />
+        <img src="images/logo.svg" id="vshop-logo" alt="logo" />
         <h1>vShop</h1>
       </header>
     );
